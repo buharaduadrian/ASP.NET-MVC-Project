c1ee9f4a6b49d35a75d735f2ec9e6abd561899ed
Radu - Adrian Buha <buharaduadrian@gmail.com>
2020-06-04 10:46:02 +0200

    Updated comic books controller to use repository.

--- src/GoThrough/Controllers/ComicBooksController.cs
+++ src/GoThrough/Controllers/ComicBooksController.cs
@@ -1,6 +1,8 @@
-﻿using GoThrough.Models;
+﻿using GoThrough.Data;
+using GoThrough.Models;
 using System;
 using System.Collections.Generic;
+using System.EnterpriseServices;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,24 +11,19 @@ namespace GoThrough.Controllers
 {
     public class ComicBooksController:Controller
     {
-        public ActionResult Detail()
-        {
+        private ComicBookRepository _comicBookRepository=null;
 
-            var comicBook = new ComicBook()
+        public ComicBooksController()
+        {
+            _comicBookRepository = new ComicBookRepository();
+        }
+        public ActionResult Detail(int? id)
+        {
+            if (id == null)
             {
-                SeriesTitle = "The Amazing Spider-Man",
-                IssueNumber = 700,
-                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus'life and his one, last, great act of revenge! Even if" +
-                        " Spider-Man survives... <strong>will Peter Parker?</strong></p>",
-                Artists = new Artist[]
-                {
-                    new Artist(){ Name = "Dan Slott", Role = "Script"},
-                    new Artist(){ Name = "Humberto Ramos", Role = "Pencils"},
-                    new Artist(){ Name = "Victor Olazaba", Role = "Inks"},
-                    new Artist(){ Name = "Edgar Delgado", Role = "Colors"},
-                    new Artist(){ Name = "Chris Eliopoulos", Role = "Letters"},
-                }
-            };
+                return HttpNotFound();
+            }
+            var comicBook = _comicBookRepository.GetComicBook(id.Value);
             return View(comicBook);
         }
     }
